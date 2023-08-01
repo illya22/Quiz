e0d843d996578418a2be8651d62a368a8e99e5f3
ILSYS <ilgyha228@gmail.com>
2023-08-02 00:18:21 +0300

    moving entities to separate project(Core)

---
+++ Core/Entities/AnswerOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class AnswerOptions : BaseEntity
+    {
+        public int QuestionId { get; set; }
+        public string AnswerText { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+}
--- DL/Data/AppDbContext.cs
+++ DL/Data/AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using DL.Entities;
+﻿using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace DL.Data
@@ -13,7 +13,7 @@ namespace DL.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Like> Likes { get; set; }
         public DbSet<Question> Questions { get; set; }
-        public DbSet<Answer_Options> Answers { get; set; }
-        public DbSet<User_Answer> UserAnswers { get; set; }
+        public DbSet<AnswerOptions> Answers { get; set; }
+        public DbSet<UserAnswer> UserAnswers { get; set; }
     }
 }
